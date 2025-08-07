f1c51e1b9c88b7f340e1e69a1633b79c6224073f
theeasylearn <theeasylearn@gmail.com>
2025-08-07 09:33:45 +0530

    api.js

--- nodejs/mongodb/api.js
+++ nodejs/mongodb/api.js
@@ -1,34 +1,78 @@
 var express = require('express');
-var {dbPromise} = require('./connection');
+var { dbPromise } = require('./connection');
 var bodyparser = require('body-parser');
 var app = express();
 
 //middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-const ROUTE = "system/";
 
-app.post(ROUTE, (req, res) => {
-    
+const ROUTE = "/system";
+
+app.post(ROUTE, (request, response) => {
+    var object = request.body;
+    console.log(object);
+    //insert as document
+    dbPromise.then((database) => {
+        database.collection('data').insertOne(object);
+        response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'data saved' }]);
+    });
 });
 
-// READ - Get all tasks
-app.get(ROUTE, (req, res) => {
-    
+// READ - Get all data
+app.get(ROUTE, (request, response) => {
+    dbPromise.then((database) => {
+        database.collection('data').find({}).toArray((error, documents) => {
+            if (error != null) {
+                response.json([{ 'error': 'error occured' }]);
+                console.log(error);
+            }
+            else 
+            {
+                documents.unshift({'error':'no'},{'total':documents.size});
+                response.json(documents);
+            }
+        });
+    })
 });
 
 // READ - Get a specific task by ID
-app.get(ROUTE+ ':id', (req, res) => {
-    ;
+app.get(ROUTE + ':id', (request, response) => {
 });
 
 // UPDATE - Update a task by ID
-app.put(ROUTE, (req, res) => {
-    
+app.put(ROUTE, (request, response) => {
+    var object = request.body;
+    var condition = { name: object.name };
+    var updateObject = { $set: { surname: object.surname, age: object.age, city: object.city } };
+    dbPromise.then((database) => {
+        database.collection('data').updateMany(condition, updateObject, function (error, result) {
+            if (error != null) {
+                response.json([{ 'error': 'error occured' }]);
+                console.log(error);
+            }
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'data updated' }]);
+            }
+        });
+    })
 });
 // DELETE - Delete a task by ID
-app.delete(ROUTE, (req, res) => {
- 
+app.delete(ROUTE, (request, response) => {
+    var object = request.body;
+    var condition = { name: object.name };
+    dbPromise.then((database) => {
+        database.collection('data').deleteOne(condition, function (error, result) {
+            if (error != null) {
+                response.json([{ 'error': 'error occured' }]);
+                console.log(error);
+            }
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'data deleted' }]);
+            }
+        });
+    });
 });
+
 app.listen(5000);
-console.log('ready to accept request');
+console.log('ready to accept requestuest');
