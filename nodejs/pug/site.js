4711deb9278e4414c041e02b538d508a42ee7ca5
theeasylearn <theeasylearn@gmail.com>
2025-08-11 09:42:28 +0530

    pug

--- nodejs/pug/site.js
+++ nodejs/pug/site.js
@@ -8,6 +8,28 @@ app.get("/one",function(request,response){
     response.render('one')
 });
 
+//create route for each and every pug 
+app.get("/two",function(request,response){
+    //redirect request to views/two.pug file 
+    response.render('two');
+});
+
+app.get("/three",function(request,response){
+    //redirect request to views/three.pug file 
+    response.render('three');
+});
+
+
+app.get("/four",function(request,response){
+    //redirect request to views/three.pug file 
+    response.render('four');
+});
+
+app.get("/five",function(request,response){
+    //redirect request to views/three.pug file 
+    response.render('five');
+});
+
 app.listen(5000);
 console.log('ready to accept request');
 
