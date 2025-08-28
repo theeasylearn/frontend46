c1c3504d30e6500f37fe91b26a3274c7a60bf034
theeasylearn <theeasylearn@gmail.com>
2025-08-28 10:46:08 +0530

    day 3 bs and fa

--- reactjs/app1/src/3_online_bs.js
+++ reactjs/app1/src/3_online_bs.js
@@ -1,23 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-//import fontAwasome and bootstrap css file
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
-// how to give comment in jsx
-var message = "Admin Login";
-var output = ( 
-        <div className="container">
-        {/* this is comment inside jsx */}
+var output = ( <div className="container">
         <div className="row mt-5">
-            { /*  this is the way we give comment inside jsx */}
             <div className="col-md-6 col-lg-6 offset-md-3 offset-lg-3">
                 <div className="card shadow">
                     <div className="card-body">
-                        <h3 className="card-title text-center mb-4">
-                            <FontAwesomeIcon icon={faUser} /> {message}
-                        </h3>
+                        <h3 className="card-title text-center mb-4">Login</h3>
                         <form>
                             <div className="mb-3">
                                 <div className="form-floating">
@@ -32,9 +20,7 @@ var output = (
                                 </div>
                             </div>
                             <div className="d-grid">
-                                <button type="submit" className="btn btn-primary">Sign In
-                                    <FontAwesomeIcon icon={faLock} />
-                                </button>
+                                <button type="submit" className="btn btn-primary">Sign In</button>
                             </div>
                         </form>
                     </div>
