75ce4ef739a11409845e7dbde8eef36b05166e24
theeasylearn <theeasylearn@gmail.com>
2025-08-29 09:36:40 +0530

    day 4 working with user defined functions

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,46 +1,26 @@
+// Develop Digital clock which will display Time & date.
 import ReactDOM from 'react-dom/client';
-
-//import fontAwasome and bootstrap css file
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
-// how to give comment in jsx
-var message = "Admin Login";
-var output = ( 
-        <div className="container">
-        {/* this is comment inside jsx */}
-        <div className="row mt-5">
-            { /*  this is the way we give comment inside jsx */}
-            <div className="col-md-6 col-lg-6 offset-md-3 offset-lg-3">
-                <div className="card shadow">
-                    <div className="card-body">
-                        <h3 className="card-title text-center mb-4">
-                            <FontAwesomeIcon icon={faUser} /> {message}
-                        </h3>
-                        <form>
-                            <div className="mb-3">
-                                <div className="form-floating">
-                                    <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" required />
-                                    <label for="emailInput">Email address</label>
-                                </div>
-                            </div>
-                            <div className="mb-3">
-                                <div className="form-floating">
-                                    <input type="password" className="form-control" id="passwordInput" placeholder="Password" required />
-                                    <label for="passwordInput">Password</label>
-                                </div>
-                            </div>
-                            <div className="d-grid">
-                                <button type="submit" className="btn btn-primary">Sign In
-                                    <FontAwesomeIcon icon={faLock} />
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+import './clock.css';
+//create object Date Library class
+let today = new Date(); 
+//create object
+let current = {
+    day:today.getDate(),
+    month:today.getMonth()+1,
+    year:today.getFullYear(),
+    hour:today.getHours(),
+    minute:today.getMinutes(),
+    second:today.getSeconds()
+}
+
+function getDateTime(now) {
+    return (<div className="full-screen">
+        <div className="time-card">
+            <div className="date">{now.day} / {now.month} / {now.year}</div>
+            <div className="time">{now.hour}:{now.minute}:{now.second}</div>
         </div>
     </div>)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(output);
+root.render(getDateTime(current)); //calling function which must return JSX
