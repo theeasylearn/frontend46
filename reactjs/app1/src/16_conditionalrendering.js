74b7fbec53532f3031d578b54ace06c9ca769c87
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-10-08 10:27:47 +0530

    Add files via upload

---
+++ reactjs/app1/src/16_conditionalrendering.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Menu from './components/Menu';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Menu />);
---
+++ reactjs/app1/src/17_conditionalrendering.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Instagram from './components/Instagram';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Instagram />);
